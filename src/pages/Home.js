40e92ec967c3268a8d5f9029a6f972eabb360ede
Haja3180 <129274543+Haja3180@users.noreply.github.com>
2023-07-11 11:49:33 +0200

    ok (sans bouton dynamique)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,6 +9,7 @@ function Home() {
     const[meals, setMeals] = useState([]);
     const[searchValue, setSearchValue] = useState("");
     const[rangeValue, setRangeValue]=useState(2);
+    const[sortMethod, setSortMethod]=useState("");
 
     const searchMeal = ()=>{
       axios.get('https://www.themealdb.com/api/json/v1/1/search.php?s='+searchValue)
@@ -37,15 +38,38 @@ function Home() {
                 </button>
             </div>
             <div className="rangeContainer">
-                <input type="range" name="" id="" />
-                <span>5</span>
+                <input 
+                    type="range" 
+                    name="" 
+                    id="" 
+                    min={1}
+                    max={24}
+                    value={rangeValue}
+                    onChange = {(e) => {setRangeValue(e.target.value)}}
+                />
+                <span>{rangeValue}</span>
             </div> 
-            <button>
-                Croissant
-            </button>
+            <div className="sortButtons">
+                <button id='croissant' onClick={()=> setSortMethod("croissant")}>
+                    Croissant
+                </button>
+                <button id='decroissant' onClick={()=> setSortMethod("decroissant")}>
+                    Decroissant
+                </button>
+            </div>
         </header>
         <section className="mealContainer">
             {meals
+                .sort((a,b)=>{
+                    if(sortMethod==="croissant"){
+                    return(a.strMeal.localeCompare(b.strMeal))
+                    }
+                    if(sortMethod==="decroissant"){
+                    return(b.strMeal.localeCompare(a.strMeal))
+                    }
+                }
+                )
+                .slice (0, rangeValue)
                 .map((meal, index)=>(<Card meal={meal} key={index}/>))
             }
         </section>
